2c10f8f2ab5897d79f58cf60d7ed74375fec5a4e
Marana Caon <65517114+maranacaon@users.noreply.github.com>
2021-08-27 18:54:03 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-lgKTcMriy4iuHoDxr9HWtxEVCNHQDPQ",
-    authDomain: "chat-labenu-d0a5f.firebaseapp.com",
-    projectId: "chat-labenu-d0a5f",
-    storageBucket: "chat-labenu-d0a5f.appspot.com",
-    messagingSenderId: "159417898397",
-    appId: "1:159417898397:web:627b84b7a98f7bf66f0b74"
+    
 };
 
 firebase.initializeApp(firebaseConfig)
