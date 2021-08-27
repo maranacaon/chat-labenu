400d56a9db002e1f828d81bb8200dbc768bd33ef
Marana Caon <maranavaal@gmail.com>
2021-08-27 18:57:27 -0300

    mudanças

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-lgKTcMriy4iuHoDxr9HWtxEVCNHQDPQ",
-    authDomain: "chat-labenu-d0a5f.firebaseapp.com",
-    projectId: "chat-labenu-d0a5f",
-    storageBucket: "chat-labenu-d0a5f.appspot.com",
-    messagingSenderId: "159417898397",
-    appId: "1:159417898397:web:627b84b7a98f7bf66f0b74"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig)
